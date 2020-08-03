3f12e1accceca61af5db3709b4bf4a7bdf3d182b
Bizan Nishimura <lipoyang1453@gmail.com>
2020-08-03 11:20:06 +0900

    clean IcsPwmTest1.ino

--- IcsPwmTest1/IcsPwmTest1.ino
+++ IcsPwmTest1/IcsPwmTest1.ino
@@ -23,18 +23,14 @@ void loop() {
   int position;
   
   for(int id=0; id<8; id++){
-    Serial.println(id);
-    Serial.println("Step1");
     for(int position=90; position>=0; position-=2){
       servos[id].setPosition(position);
       delay(20);
     }
-    Serial.println("Step2");
     for(int position=0; position<=180; position+=2){
       servos[id].setPosition(position);
       delay(20);
     }
-    Serial.println("Step3");
     for(int position=180; position>=90; position-=2){
       servos[id].setPosition(position);
       delay(20);
