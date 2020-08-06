70cecb3e860d93822e6c323bcc25d541b320af85
Bizan Nishimura <lipoyang1453@gmail.com>
2020-08-07 08:04:09 +0900

    modify test code

--- IcsPwmTest1/IcsPwmTest1.ino
+++ IcsPwmTest1/IcsPwmTest1.ino
@@ -22,6 +22,8 @@ void loop() {
   
   int position;
   
+#if 1
+  // (1) position parameter 3500 - 11500 (ICS compatible mode)
   for(int id=0; id<8; id++){
     for(int position=7500; position>=3500; position-=100){
       servos[id].setPosition(position);
@@ -36,4 +38,21 @@ void loop() {
       delay(20);
     }
   }
+#else
+  // (2) position parameter 500 - 2500 (microsecond mode)
+  for(int id=0; id<8; id++){
+    for(int position=1500; position>=600; position-=25){
+      servos[id].setPosition(position);
+      delay(20);
+    }
+    for(int position=600; position<=2400; position+=25){
+      servos[id].setPosition(position);
+      delay(20);
+    }
+    for(int position=2400; position>=1500; position-=25){
+      servos[id].setPosition(position);
+      delay(20);
+    }
+  }
+#endif
 }
